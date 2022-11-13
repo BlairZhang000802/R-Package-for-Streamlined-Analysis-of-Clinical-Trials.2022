bbb5f1535ead6492a07d73d6e3b9e3e516b59760
Bo Zhang <z000080b@163.com>
2022-11-13 14:23:15 -0500

    edit test_spectral.R to make coverage to 100%

--- tests/testthat/test-spectral.R
+++ tests/testthat/test-spectral.R
@@ -1,10 +1,24 @@
 test_that(
-  "The accel_plot() returns a ggplot object.",
+  "The spectral_signature() returns a data.frame.",
   {
     data(ukb_accel)
     p <-  ukb_accel[1:100, ] |>
-      spectral_signature(take_log = TRUE) |>
-      accel_plot()
-    expect_true(inherits(p, "gg"))
+      spectral_signature()
+    expect_s3_class(p, "data.frame")
+  }
+)
+
+test_that(
+  "The spectral_signature() takes log when take-log = TRUE.",
+  {
+    data(ukb_accel)
+    p <-  ukb_accel[1:100, ] |>
+      bis620.2022:: spectral_signature(take_log = TRUE) |>
+      select(X, Y, Z)
+    n <- ukb_accel[1:100, ] |>
+      bis620.2022:: spectral_signature() |>
+      select(X, Y, Z) |>
+      log()
+    expect_equal(p, n)
   }
 )
